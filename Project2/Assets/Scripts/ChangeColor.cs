3650d68869119a52afa8bb1297c5cda50a5daf0a
Chris Schmidt <christoffer@allright.dk>
2018-02-12 17:44:03 +0100

    Chris breaks stuff maybe

--- Project2/Assets/Scripts/ChangeColor.cs
+++ Project2/Assets/Scripts/ChangeColor.cs
@@ -18,7 +18,7 @@ public class ChangeColor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Runway == null)
+		if (Runway == null)
         {
             // change color to transparent
             GetComponent<Renderer>().material.color = Color.red;
---
+++ Project2/Assets/Scripts/LocalCoordTransformer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalCoordTransformer : MonoBehaviour {
+
+	public GameObject toTransform;
+	private string guiLabel = "Nothing Yet";
+
+	private void OnGUI() {
+		GUI.color = Color.black;
+		GUI.Label (new Rect (10, 10, 500, 100), guiLabel);
+	}
+
+	private bool isWithinBounds(Matrix4x4 result){
+		float x = result.m03;
+		float z = result.m23;
+		float y = result.m13;
+		Debug.Log ((Mathf.Pow (x, 2) + Mathf.Pow (z, 2)));
+		return (Mathf.Pow (x, 2) + Mathf.Pow (z, 2)) < 0.2f && y < 40;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Matrix4x4 subjectTransform = toTransform.transform.localToWorldMatrix;
+		Matrix4x4 inv = transform.localToWorldMatrix.inverse;
+		Matrix4x4 res = inv * subjectTransform;
+		guiLabel = res.ToString ();
+		if(isWithinBounds(res)) {
+			if (res.m23 > 0) {
+				guiLabel += "\nHemisphere: North";
+			} else {
+				guiLabel += "\nHemisphere: South";
+			}
+		}
+	}
+}
